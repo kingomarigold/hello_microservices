f3e41438a440bad02221b0117dbdf8ef9eb45ddf
kingomarigold <karthiksr@virtusa.com>
2016-09-08 08:27:13 +0530

    Fixes for gateway.

---
+++ gateway/src/main/groovy/com/karthiksr/demo/gateway/GatewayApplication.groovy
@@ -0,0 +1,22 @@
+package com.karthiksr.demo.gateway
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class])
+class GatewayApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run GatewayApplication, args
+	}
+}
